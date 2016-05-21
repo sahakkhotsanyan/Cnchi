dc1101b541aab1a2f8a97733db700bb818fd1709
sahakkhotsanyan <sahakjab@gmail.com>
2016-05-21 11:50:50 +0400

    Install dependencies
    installs dependencies for run cnchi

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #! /bin.bash
 
 pacman -Syu gtk3 python3 python-cairo python-dbus python-gobject python-mako python-requests pyparted parted dosfstools mtools ntfs-3g ntfsprogs pyalpm alpm webkit2gtk upower encfs pam_encfs iso-codes clutter clutter-gtk clutter-gst cheese
+
